cd01ef0ba682f0f468ce0f4cb8d1b345dc104459
3bdoelnaggar <3bdoelnaggar@gmail.com>
2021-06-24 19:02:43 +0200

    create name age ui logic

--- app/src/main/java/com/elnaggar/inovatask/ui/nameAgeList/NameAgeListFragment.kt
+++ app/src/main/java/com/elnaggar/inovatask/ui/nameAgeList/NameAgeListFragment.kt
@@ -1,20 +1,18 @@
-package com.elnaggar.inovatask.ui
+package com.elnaggar.inovatask.ui.nameAgeList
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.navigation.fragment.findNavController
-import com.elnaggar.inovatask.R
-import com.elnaggar.inovatask.databinding.FragmentFirstBinding
+import com.elnaggar.inovatask.databinding.NameAgeListFragmentBinding
 
 /**
  * A simple [Fragment] subclass as the default destination in the navigation.
  */
-class FirstFragment : Fragment() {
+class NameAgeListFragment : Fragment() {
 
-    private var _binding: FragmentFirstBinding? = null
+    private var _binding: NameAgeListFragmentBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -25,7 +23,7 @@ class FirstFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
 
-        _binding = FragmentFirstBinding.inflate(inflater, container, false)
+        _binding = NameAgeListFragmentBinding.inflate(inflater, container, false)
         return binding.root
 
     }
@@ -33,9 +31,6 @@ class FirstFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        binding.buttonFirst.setOnClickListener {
-            findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
-        }
     }
 
     override fun onDestroyView() {
---
+++ app/src/main/java/com/elnaggar/inovatask/ui/nameAgeList/NameAgeListViewModel.kt
@@ -0,0 +1,50 @@
+package com.elnaggar.inovatask.ui.nameAgeList
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.elnaggar.inovatask.data.entity.NameAge
+import com.elnaggar.inovatask.data.repository.NameAgeRepository
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class NameAgeListViewModel @Inject constructor(private val nameAgeRepository: NameAgeRepository) :
+    ViewModel() {
+
+    private val _stateLiveData = MutableLiveData<NameAgeListState>()
+    val staLiveData: LiveData<NameAgeListState> = _stateLiveData
+
+    fun getNameAgeList() {
+        _stateLiveData.value = NameAgeListState.Loading
+        viewModelScope.launch {
+            try {
+                val result = nameAgeRepository.getNameAgeList()
+                val uiList = result.map {
+                    it.toUiNameAge()
+                }
+                _stateLiveData.value = NameAgeListState.Success(uiList)
+            } catch (exception: Exception) {
+                _stateLiveData.value = NameAgeListState.Error
+            }
+
+        }
+    }
+}
+
+private fun NameAge.toUiNameAge(): UiNameAge {
+    val uiName = "Name: $name"
+    val uiAge = "Age: $age"
+    return UiNameAge(uiName, uiAge)
+}
+
+sealed class NameAgeListState {
+    data class Success(val data: List<UiNameAge>) : NameAgeListState()
+    object Loading : NameAgeListState()
+    object Error : NameAgeListState()
+}
+
+data class UiNameAge(val name: String, val age: String)
+
